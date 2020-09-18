3d173e96f7cecc1c78b8064b7ab02abc507ed268
Dayton <daytonlowell@me.com>
2020-09-17 22:19:50 -0500

    Now actually does the formatting

--- src/main.js
+++ src/main.js
@@ -3,18 +3,16 @@ const kebab = require('just-kebab-case')
 const snake = require('just-snake-case')
 const pascal = require('just-pascal-case')
 
-nova.commands.register("camel", (editor) => {
-    console.log(Object.keys(editor).length)
-})
+function format(editor, formatter) {
+    editor.edit(e => {
+        for (const range of editor.selectedRanges) {
+            const text = editor.getTextInRange(range)
+            e.replace(range, formatter(text))
+        }
+    })
+}
 
-nova.commands.register("kebab", (editor) => {
-    console.log(Object.keys(editor).length)
-})
-
-nova.commands.register("snake", (editor) => {
-    console.log(Object.keys(editor).length)
-})
-
-nova.commands.register("pascal", (editor) => {
-    console.log(Object.keys(editor).length)
-})
+nova.commands.register("camel", editor => format(editor, camel))
+nova.commands.register("kebab", editor => format(editor, kebab))
+nova.commands.register("snake", editor => format(editor, snake))
+nova.commands.register("pascal", editor => format(editor, pascal))
